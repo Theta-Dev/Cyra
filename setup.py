89148acfb40ab0a26f1f16bbe45cef344b8a56d4
Theta-Dev <t.testboy@gmail.com>
2021-05-25 10:53:56 +0200

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as f:
 
 setuptools.setup(
     name='Cyra',
-    version='1.0.0',
+    version='1.0.1',
     author='ThetaDev',
     description='Cyra is a simple config framework for Python.',
     long_description=README,
